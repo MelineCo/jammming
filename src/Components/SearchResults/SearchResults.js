86ff44d3789c1a75ddb5481bcfe61fb0155903a7
Emeline Courcelle <emelinecourcelle@MacBook-Air-de-Emeline.local>
2023-09-01 23:23:49 +0200

    Modify Result component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './SearchResults.module.css';
+import Result from '../Track/Result';
 
 function SearchResults( {results} ) {
   return (
     <div className={styles.resultsList}>
       {results.map((result, id) => {
-          return <div key={id}>{result.name}</div>;
+          return <Result result={result} key={id} />;
         })}
+        <p>A</p>
     </div>
   )
 }
---
+++ src/Components/Track/Result.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Result = ({result}) => {
+  return (
+    <div>
+        <div>{result.name}</div>
+       <div>B</div>
+
+    </div>
+  )
+}
+
+export default Result
