ed549fc0949f78f67ac62d0784927cf90cbc5372
Emeline Courcelle <emelinecourcelle@MacBook-Air-de-Emeline.local>
2023-09-01 15:43:23 +0200

    Init tracklist

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Tracks from './Tracks';
 
 function Tracklist( { tracks }) {
   return (
     <div className="track-list">
-      
+      {tracks.map(track => {
+        return <Tracks track={track} key={track.id} />;
+      } )}
     </div>
   )
 }
